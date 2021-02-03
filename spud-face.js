e06e02e039ef6a87d9e6b808df70f980d676ab53
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-02-03 15:24:21 -0600

    first commit

---
+++ spud-face.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+
+
+  const licenseForm = document.getElementById("drivers-license-form")
+  
+
+
+  const submitForm = document.getElementsByClassName("form_submit")
+  licenseForm.addEventListener("submit", event => {
+    console.log("submitted")
+    event.preventDefault("submit");      
+      if()
+  });
+
+  // ** Phase 1B: Update license with event delegation and event.target ** 
+
+
+  // ** Phase 2: Add focus and blur events to form inputs **
+ 
+
+  // ** Phase 3: Check that license numbers match **
+ 
+
+  // ** Phase 4: Update submit button click count **
+
+ 
+});

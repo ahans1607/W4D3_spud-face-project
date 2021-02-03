99db20f8a1e54e20d1011bbbd77e79d87ac504b7
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-02-03 17:05:35 -0600

    commit 3?

--- spud-face.js
+++ spud-face.js
@@ -1,17 +1,35 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
   const licenseForm = document.getElementById("drivers-license-form")
-  console.log(licenseForm);
-  let formfields = document.getElementsByClassName("form__input");
-  console.log(formfields);
-  const license = document.querySelectorAll(".license__info");
-  console.log(license);
-  const submitForm = document.getElementsByClassName("form_submit");
-  licenseForm.addEventListener("submit", event => {
-    console.log("submitted")
-    // event.preventDefault("submit");
+  // console.log(licenseForm);
 
-  });
+  let formFields = document.getElementsByClassName("form__input");
+  // console.log(formFields);
+  const license = document.querySelectorAll(".license__info");
+  // console.log(license);
+  const submitForm = document.getElementsByClassName("form_submit");
+  // console.log(submitForm);
+
+  
+
+  for (let i = 0; i < formFields.length; i++){
+    inputField = formFields[i]
+    let arr = Array.from(inputField) 
+    console.log(arr)
+    // console.log(inputField)
+      for(let input of arr){
+       console.log(input);
+      }
+
+    
+  }
+
+
+  // licenseForm.addEventListener("submit", event => {
+  //   console.log("submitted")
+  //   // event.preventDefault("submit");
+
+  // });
 
   // ** Phase 1B: Update license with event delegation and event.target **
 

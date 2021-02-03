bcd962322781e0be5cb878be4bfca26d844a975d
jdaniel01 <professor4cob@outlook.com>
2021-02-03 15:21:49 -0800

    another push

--- spud-face.js
+++ spud-face.js
@@ -10,26 +10,27 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const submitForm = document.getElementsByClassName("form_submit");
   // console.log(submitForm);
 
-  
+  licenseForm.addEventListener("focus", event => {
+    event.target.style.backgroundcolor = 'lightgreen'
 
-  for (let i = 0; i < formFields.length; i++){
-    inputField = formFields[i]
-    let arr = Array.from(inputField) 
-    console.log(arr)
-    // console.log(inputField)
-      for(let input of arr){
-       console.log(input);
-      }
-
-    
-  }
+  }, true);
+  licenseForm.addEventListener("blur", event => {
 
 
-  // licenseForm.addEventListener("submit", event => {
-  //   console.log("submitted")
-  //   // event.preventDefault("submit");
+  });
+  licenseForm.addEventListener("input", event => {
 
-  // });
+
+  });
+  licenseForm.addEventListener("submit", event => {
+
+
+  });
+  licenseForm.addEventListener("click", event => {
+
+  });
+
+
 
   // ** Phase 1B: Update license with event delegation and event.target **
 
@@ -44,3 +45,21 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
 
 });
+// for (let i = 0; i < formFields.length; i++){
+  //   inputField = formFields[i]
+  //   let arr = Array.from(inputField)
+  //   console.log(arr)
+  //   // console.log(inputField)
+  //     for(let input of arr){
+  //      console.log(input);
+  //     }
+
+
+}
+
+
+// licenseForm.addEventListener("submit", event => {
+//   console.log("submitted")
+//   // event.preventDefault("submit");
+
+// });

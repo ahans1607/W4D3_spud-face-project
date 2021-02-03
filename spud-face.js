6dc7b13f7d21c05f81884d1246c9636182da1c8c
jdaniel01 <professor4cob@outlook.com>
2021-02-03 13:44:08 -0800

    checking

--- spud-face.js
+++ spud-face.js
@@ -1,27 +1,28 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
-
+  console.log("spudhead");
   const licenseForm = document.getElementById("drivers-license-form")
-  
+  console.log(licenseForm);
 
-
-  const submitForm = document.getElementsByClassName("form_submit")
+  const textElements = document.querySelectorAll(".license__info");
+  console.log(textElements);
+  const submitForm = document.getElementsByClassName("form_submit");
   licenseForm.addEventListener("submit", event => {
     console.log("submitted")
-    event.preventDefault("submit");      
-      if()
+    // event.preventDefault("submit");
+
   });
 
-  // ** Phase 1B: Update license with event delegation and event.target ** 
+  // ** Phase 1B: Update license with event delegation and event.target **
 
 
   // ** Phase 2: Add focus and blur events to form inputs **
- 
+
 
   // ** Phase 3: Check that license numbers match **
- 
+
 
   // ** Phase 4: Update submit button click count **
 
- 
+
 });
